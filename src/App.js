d01b7da671a36474a148ebb816e2c5c8b625f54d
arif99050 <arifulislam99050@gmail.com>
2022-03-22 11:07:39 +0600

    create componet and fetch API data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Arif></Arif>
+      
+      
     </div>
   );
 }
+function Arif (){
+  
+    const [users, setUsers]=useState([]);
+    useEffect( ()=>{
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res=>res.json())
+      .then(data=>setUsers(data))
+    },[])
+
+  
+   return (
+    <div>
+      
+      {
+        users.map(user => <User name={user.name} email={user.email}></User> )
+      }
+    </div>
+  )
+}
+function User(props){
+  return(
+    <div>
+      <h2> Name : {props.name}</h2>
+      <p>Email {props.email}</p>
+
+    </div>
+  )
+}
+
 
 export default App;
